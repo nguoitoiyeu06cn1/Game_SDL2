03326eadd0281bed90a37b8401004db87af69d6b
nguoitoiyeu06cn1 <ngocdungvinhquang2@gmail.com>
2025-03-30 17:52:27 +0700

    final_x4

--- render.cpp
+++ render.cpp
@@ -8,18 +8,18 @@ using namespace std;
 }*/
 
 void print_lose( SDL_Renderer*& renderer, TTF_Font*& font){
-    renderText(renderer, font, "Game Over!", SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2);
-    renderText(renderer, font, "Do you want to play again?", SCREEN_WIDTH / 2 - 110, SCREEN_HEIGHT / 2 + 30);
-    renderText(renderer, font, "Choose Y: yes, N: no", SCREEN_WIDTH / 2 - 90, SCREEN_HEIGHT / 2 + 60);
+    renderText(renderer, font, "Game Over!", SCREEN_WIDTH / 2 - 60, SCREEN_HEIGHT / 2 - 60);
+    renderText(renderer, font, "Do you want to play again?", SCREEN_WIDTH / 2 - 110, SCREEN_HEIGHT / 2 - 30);
+    renderText(renderer, font, "Choose Y: yes, N: no", SCREEN_WIDTH / 2 - 90, SCREEN_HEIGHT / 2 );
     saveHighScore(score);  //Save high score when game is over
     saveHighLevel(level); // Save high level when game is over
 }
 void print_level_up(SDL_Renderer*& renderer, TTF_Font*& font)
 {
-    renderText(renderer, font, "Game Over!", SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2);
-    renderText(renderer, font, "Your score is eligible to move up to a new level. ", SCREEN_WIDTH / 2 - 10, SCREEN_HEIGHT / 2 + 0);
-    renderText(renderer, font, "Do you want to play next level?", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2 + 30);
-    renderText(renderer, font, "Choose Y: yes, N: no", SCREEN_WIDTH / 2 - 90, SCREEN_HEIGHT / 2 + 60);
+    renderText(renderer, font, "Game Over!", SCREEN_WIDTH / 2 - 60, SCREEN_HEIGHT / 2 - 60);
+    renderText(renderer, font, "Your score is eligible to move up to a new level. ", SCREEN_WIDTH / 2 - 200, SCREEN_HEIGHT / 2 - 30);
+    renderText(renderer, font, "Do you want to play next level?", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2 );
+    renderText(renderer, font, "Choose Y: yes, N: no", SCREEN_WIDTH / 2 - 90, SCREEN_HEIGHT / 2 + 30);
     saveHighScore(score);  //Save high score when game is over
     saveHighLevel(level); // Save high level when game is over
 }
